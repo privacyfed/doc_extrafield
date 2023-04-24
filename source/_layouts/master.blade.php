11db3f982c0732fd09d421e49bb189841b55625a
marco76tv <marco.sottana@gmail.com>
2023-04-24 18:23:40 +0200

    .

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -24,7 +24,7 @@
     <title>{{ $page->siteName }}{{ $page->title ? ' | ' . $page->title : '' }}</title>
 
     <link rel="home" href="{{ $page->baseUrl }}">
-    <link rel="icon" href="/favicon.ico">
+    <link rel="icon" href="/extra_field/favicon.ico">
 
     @stack('meta')
 
@@ -67,7 +67,7 @@
         @yield('body')
     </main>
 
-    <script src="{{ mix('js/main.js', 'assets/build') }}"></script>
+    <script src="/extra_field{{ mix('js/main.js', 'assets/build') }}"></script>
 
     @stack('scripts')
 
